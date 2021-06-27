ffc7fec557af75b03adef9979b6dee4cbff42af8
Keith <keith.kny@gmail.com>
2021-06-27 20:14:54 +0100

    Move Queue

---
+++ Concentration/Assets/src/Controls/Directions/MoveQueue.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class MoveQueue
+{
+    private Queue _moveQueue;
+
+    public MoveQueue()
+    {
+        _moveQueue = new Queue();
+    }
+
+    public void Enqueue(Directions nextMove)
+    {
+        if(nextMove != Directions.NonePressed && nextMove != Directions.Undefined)
+        {
+            Debug.Log($"Added direction: {nextMove}");
+            _moveQueue.Enqueue(nextMove);
+        }
+    }
+
+    public Directions Dequeue()
+    {
+        try
+        {
+            var direction = (Directions)_moveQueue.Dequeue();
+            return direction;
+        } catch(Exception e)
+        {
+            //TODO: Add logger
+
+            //TODO: Sort this
+            return Directions.NonePressed;
+        }
+    }
+}
--- Concentration/Assets/src/Controls/MovementController.cs
+++ Concentration/Assets/src/Controls/MovementController.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
+    [SerializeField]
+    private MoveQueue _moveQueue;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _moveQueue = new MoveQueue();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Done before any processing
+        _moveQueue.Enqueue(GetNextMove());
+
+        //Next Move
+        var currentMove = _moveQueue.Dequeue();
+        ApplyMove(currentMove);
+
+
     }
+
+    private Directions GetNextMove()
+    {
+        var rightKeyPressed = PlayerConfig.GetRightKeyPressed();
+        var leftKeyPressed = PlayerConfig.GetLeftKeyPressed();
+
+        if(rightKeyPressed)
+        {
+            return Directions.Right;
+        }
+
+        if(leftKeyPressed)
+        {
+            return Directions.Left;
+        }
+
+        return Directions.NonePressed;
+    }
+
+    private void ApplyMove(Directions move)
+    {
+        Debug.Log($"Applied move: {move}");
+    }
+
 }
