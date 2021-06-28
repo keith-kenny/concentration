548f32edb84f98c4fa8f4a822ee06ab272daf05e
Keith <keith.kny@gmail.com>
2021-06-28 21:16:57 +0100

    starting obstacle management

--- Concentration/Assets/src/Environment/Obstacle.cs
+++ Concentration/Assets/src/Environment/Obstacle.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    [SerializeField]
+
     // Start is called before the first frame update
     void Start()
     {
--- Concentration/Assets/src/Environment/RowManager.cs
+++ Concentration/Assets/src/Environment/RowManager.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class RowManager : MonoBehaviour
 {
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private List<Row> rows;
+    [SerializeField]
+    private object locker;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rows = new List<Row>();
     }
 
     // Update is called once per frame
@@ -15,4 +21,34 @@ public class RowManager : MonoBehaviour
     {
         
     }
+
+
+    private void GenerateRow()
+    {
+        lock(locker)
+        {
+            
+        }
+    }
+
+    private void AdvanceSpeed()
+    {
+    }
+
+    private void ManageSingleRow(Row row)
+    {
+
+    }
+
+    private void ManageRows()
+    {
+        lock (locker)
+        {
+            foreach (var row in rows)
+            {
+                ManageSingleRow(row);
+            }
+        }
+    }
+
 }
